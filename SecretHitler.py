38a4dd7771a71d62b1da25029586566b724f57dc
Brian Lenz <maker772@gmail.com>
2019-04-05 02:30:47 -0500

    expanded player manager, documentation, and started chancellor voting

--- SecretHitler.py
+++ SecretHitler.py
@@ -43,7 +43,10 @@ class SecretHitler(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.command(name='joinSH')
     async def makePlayer(self, context):
         playerManager.joinLobby(context.author.id)
-        playerManager.makeFakePlayers(4)
+
+    @commands.command(name='fake')
+    async def makeFakePlayers(self, context, num=4):
+        playerManager.makeFakePlayers(num)
 
     @commands.command(name='playerlist')
     async def playerList(self, context):
@@ -60,11 +63,10 @@ class SecretHitler(commands.Cog, command_attrs=dict(hidden=True)):
 
     #HELPER COMMAND
     @commands.command(name='testsh')
-    async def testingSecretHitlerCommand(self, context):
-        joinLobby(context.author.id)
-        makeFakePlayers(context, 4)
-        evaluateAndAssignPlayerRoles(context)
-        await context.send(printPlayerOrder())
+    async def testingSecretHitlerCommand(self, context, num=4):
+        playerManager.joinLobby(context.author.id)
+        playerManager.makeFakePlayers(num)
+        playerManager.assignPlayerRoles(context)
 
 class AsciiShape(object):
 
@@ -179,23 +181,62 @@ class CardManager():
         return gameBoard
 
 class PlayerManager():
+    """Manages all Player related components.
+
+    This manager is used as a single container for keeping
+    track of players. This includes the current position,
+    president, and chancellor.
+
+    Attributes
+    ----------
+    currentPosition : int
+        the current position in the playerList
+    playerList : list
+        contains a list of Player objects that have been
+        assigned their attributes (role, position, etc)
+    lobbyList : list
+        contains a list of user_ids that are ready to be
+        added to a game
+    candidate_pre : int
+        the position of the Presidential Candidate (voting phase)
+    candidate_cha : int
+        the position of the Chancellor Candidate (voting phase)
+    president : int
+        the position of the elected President (play phase)
+    chancellor : int
+        the position of the elected Chancellor (play phase)
+
+    """
 
     def __init__(self):
         self.currentPosition = -1
         self.playerList = []
         self.lobbyList = []
+        self.candidate_pre = -1
+        self.candidate_cha = -1
+        self.president = -1
+        self.chancellor = -1
+        self.votes_recorded = 0
 
     def sortPlayerList(self):
+        """Sort the playerList by the Player's positions"""
+
         self.playerList.sort(key=lambda x: x.position)
 
     def joinLobby(self, playerID):
+        """Append a playerID to the lobbyList"""
+
         self.lobbyList.append(playerID)
 
     def makeFakePlayers(self, num: int):
+        """Temporary helper function for populating playerList."""
+
         for i in range(num):
             self.joinLobby(1 + i)
 
     def printPlayerOrder(self):
+        """Creates a simple string including all players in playerList and their name"""
+
         message = '```'
         for i in range(len(self.playerList)):
             message += 'Player ' + str(i+1) + ': ' + self.playerList[i].name + '\n'
@@ -203,25 +244,26 @@ class PlayerManager():
         return message
 
     def advancePlayerPosition(self):
-        self.currentPosition += 1
+        """Advances the current position by one, and returns the new position"""
+
+        self.currentPosition += 1 # Increase current position
 
         if (self.currentPosition) in range(len(self.playerList)):
             if not self.playerList[self.currentPosition].dead:
-                print('in bounds and not dead')
-                print('new current is ' + str(self.currentPosition))
+                print('New Current Position: ' + str(self.currentPosition))
                 return self.currentPosition
             else:
-                print('current is dead, advance')
                 advancePlayerPosition()
         else:
-            print('reached the end of the player list')
-            self.currentPosition += 1 # reset to the start of the list
+            self.currentPosition = -1 # reset to the start of the list
             advancePlayerPosition()
 
-    # Count the players in lobby and assign all to the playerList with role, party, and position
     def assignPlayerRoles(self, context):
+        """Takes all users in the lobbyList and turns them into Players for the playerList"""
+
         playerCount = len(self.lobbyList)                                       # Helper for player count
         if playerCount in range(5, 10+1):                                       # Check for valid number of players, 5-10
+            self.playerList.clear()                                             # Clear any old playerList
             roles = roleManager.makeRoleListForAssignment(playerCount)          # Assign the appropriate role distribution
             random.shuffle(roles)                                               # Shuffle to feel better
             positions = list(range(playerCount))                                # Make a position list of the total player count
@@ -232,8 +274,7 @@ class PlayerManager():
                 positionAssignment = random.choice(positions)                   # Choose a random position
                 if roleAssignment is 'Liberal': partyAssignment = 'Liberal'     # Assign the party
                 else: partyAssignment = 'Fascist'
-            
-                
+
                 user = context.bot.get_user(playerID)
                 if user is None: name = 'Fake ' + str(playerID)
                 else: name = user.name
@@ -244,10 +285,19 @@ class PlayerManager():
                 roles.remove(roleAssignment)                                    # Remove the role from temporary list
                 positions.remove(positionAssignment)                            # Remove the position from temporary list
             self.sortPlayerList()
+            self.lobbyList.clear()
         else:
             print('Player count invalid: ' + str(playerCount))
 
+    def setElectionCandidates(self, president: int, chancellor: int):
+        """Set the presidential and chancellor candidates"""
+
+        self.candidate_pre = president
+        self.candidate_cha = chancellor
+
 class Player(object):
+    """Acts as a container for all attributes required for a Player"""
+
     def __init__(self, name='Empty', id='-1', party='Unassigned', role='Unassigned', position=-1, ready=False, term_limited=False, dead=False):
         self.name = name
         self.id = id
@@ -304,11 +354,12 @@ admin commands (start, end)
     after lobby is created
 presidential powers
     examine player, bullet, top cards, veto, select next president
-president selection conditions (can't pick previous etc)
 
-player assignment
-    take number of players and select appropriate distribution and assign
-determine player order
+bind these commands to specific channels
+perhaps have a designated channel for bot displays (board, votes, etc)
+maybe look into having player votes and selections done not in dms, but "private" channels
+    mainly for easier access, instead of having to jump into and out of the DM windows
+
 '''
 
 # One loop every TICK_RATE seconds
@@ -317,8 +368,6 @@ TICK_RATE = 2
 cardManager = CardManager(17)
 playerManager = PlayerManager()
 roleManager = RoleManager()
-
-currentPosition = -1
 
 # Number emoji's
 NUMBER_EMOJI = ['1\N{combining enclosing keycap}',
@@ -337,7 +386,11 @@ NUMBER_EMOJI = ['1\N{combining enclosing keycap}',
 
 # start a president selection
 # returns the message created
+# needs to be forced as a DM to the Presidential Candidate
 async def prepareChancellorSelectionPrompt(context):
+    """Creates and sends the Chancellor Selection Prompt for the Presidential Candidate.
+    Returns the created message."""
+
     # send a DM to presidential candidate
     # prompt with all players in the game (including availability (dead or term limit))
 
@@ -349,7 +402,7 @@ async def prepareChancellorSelectionPrompt(context):
         if playerManager.playerList[i].term_limited:
             prompt += '\t-\t Term-Limited'
         if playerManager.playerList[i].position is playerManager.currentPosition:
-            prompt += '\t-\t President Elect'
+            prompt += '\t-\t Presidential Candidate'
 
     msg = await context.send(prompt)
     
@@ -360,33 +413,33 @@ async def prepareChancellorSelectionPrompt(context):
 
 # Handles evaluation of the Chancellor selection
 # Takes argument of the chancellorSelectionPrompt message
-# Should be used inside a DM
+# msg should be from inside a DM
 # Removes the voting prompt after a valid selection
 # Returns position of the selected Chancellor as an int
 # ::FIX:: need to add confirmation for selection
 async def waitForChancellorSelectionReaction(context, msg):
+    """Waits for a reaction on the designated message and returns it as an int"""
+
     # Ignore the bot reactions, and ensure the reaction is a number selection
     def check(reaction, user):
-        return user != msg.author and reaction.emoji in NUMBER_EMOJI
-    print('Waiting for reaction')
-    reaction, user = await context.bot.wait_for('reaction_add', check=check)
+        return not user.bot and reaction.emoji in NUMBER_EMOJI
+    reaction, user = await context.bot.wait_for('reaction_add', check = check)
 
     # Iterate through keys to determine actual number being represented
     # Check if that player is a valid selection for Chancellor
     for i in range(len(NUMBER_EMOJI)):
-        print(str(i))
         if NUMBER_EMOJI[i] == reaction.emoji:
             if i is playerManager.currentPosition:
-                await msg.remove_reaction(reaction.emoji, context.author)
-                await context.send('You can\'t elect yourself! Please vote again.', delete_after=5)
+                await msg.remove_reaction(reaction.emoji, user)
+                await context.send('You can\'t elect yourself! Please vote again.', delete_after = 5)
                 return await waitForChancellorSelectionReaction(context, msg)
             elif i is playerManager.playerList[i].dead:
-                await msg.remove_reaction(reaction.emoji, context.author)
-                await context.send('That player is dead! Please vote again.', delete_after=5)
+                await msg.remove_reaction(reaction.emoji, user)
+                await context.send('That player is dead! Please vote again.', delete_after = 5)
                 return await waitForChancellorSelectionReaction(context, msg)
             elif i is playerManager.playerList[i].term_limited:
-                await msg.remove_reaction(reaction.emoji, context.author)
-                await context.send('That player is term-limited for this turn. Please vote again.', delete_after=5)
+                await msg.remove_reaction(reaction.emoji, user)
+                await context.send('That player is term-limited for this turn. Please vote again.', delete_after = 5)
                 return await waitForChancellorSelectionReaction(context, msg)
             else:
                 await msg.delete()
@@ -395,21 +448,73 @@ async def waitForChancellorSelectionReaction(context, msg):
         await context.send('Something has gone horribly wrong')
         return None
 
-async def prepareChancellorVotingPrompt(context, chancellorPos: int):
-    # send PMs to all alive players
-    # prompt for ja or nein
-    # return a list of all messages used
+async def prepareChancellorVotingPrompt(context):
+    """Creates and sends a message to all living players with a voting ballot
+    
+    Returns a list of all Messages created"""
 
-    prompt = 'Voting: ' + str(playerManager.currentPosition) + '\t' + str(playerManager.chancellorPos)
-    await context.send(prompt)
+    prompt = 'Voting for Candidates: President - ' + str(playerManager.candidate_pre) + '\tChancellor - ' + str(playerManager.candidate_cha)
+    msgList = []
+    # Iterate through Players in playerList
+    for p in playerManager.playerList:
+        if not p.dead: 
+            # If they are alive, give them a voting ballot ::FIX:: move this to DMs?
+            msgList.insert(p.position, await context.send(p.name + ' vote ballot\n' + prompt))
+            await msgList[p.position].add_reaction('👍')
+            await msgList[p.position].add_reaction('👎')
+        else: print(p.name + ' is dead. No vote.')
     return msgList
 
-async def initVotingSequence(context):
-    print(str(playerManager.advancePlayerPosition()))
-    msg = await prepareChancellorSelectionPrompt(context)
-    selection = await waitForChancellorSelectionReaction(context, msg)
+async def waitForCandidateVoteReaction(context, msg):
+    """Waits for a reaction to a Candidate vote on a ballot.
+    
+    ::FIX:: Needs to return the vote choice, possibly the message/voter
+    Alter to wait for simply the confirmation and then count vote"""
 
-    await context.send('Candidate Number ' + str(selection + 1) + ' selected')
+    def check(reaction, user):
+        return not user.bot and reaction.emoji in ['👍', '👎'] and reaction.message.id == msg.id
+
+    reaction, user = await context.bot.wait_for("reaction_add", check = check)
+    playerManager.votes_recorded += 1
+    await context.send('Votes recorded: {}'.format(playerManager.votes_recorded))
+
+
+    confirmMessage = await context.send('confirm selection {} {}'.format(reaction.emoji, user.name))
+    await confirmMessage.add_reaction('👍')
+    await confirmMessage.add_reaction('👎')
+    def check2(reaction, user):
+        return not user.bot and reaction.emoji in ['👍', '👎'] and reaction.message.id == confirmMessage.id
+
+    await context.bot.wait_for("reaction_add", check = check2)
+    print('Confirmed vote')
+
+    return reaction, user
+
+async def initVotingSequence(context):
+    """Includes entire sequence of voting functions.
+    
+        1. Advances playerPosition
+        2. Creates the Selection Prompt for Chancellor
+        3. Waits for the Chancellor Selection
+        4. Sets the Candidates
+        5. Creates the Voting Prompts for the Candidates
+        6. Waits on all reactions from voting
+    """
+
+    print(str(playerManager.advancePlayerPosition()))
+    chancellorSelectMessage = await prepareChancellorSelectionPrompt(context)
+    chancellorCandidatePosition = await waitForChancellorSelectionReaction(context, chancellorSelectMessage)
+    playerManager.setElectionCandidates(playerManager.currentPosition, chancellorCandidatePosition)
+    chancellorVotingList = await prepareChancellorVotingPrompt(context)
+    tasks = []
+    for m in chancellorVotingList:
+        tasks.append(waitForCandidateVoteReaction(context, m))
+
+    results = await asyncio.gather(*tasks)
+
+    print(results[0])
+
+    print('Voting finished')
 
 # Main loop for Secret Hitler
 # Handle display drawing
